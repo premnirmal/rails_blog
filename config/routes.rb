6149af4357c5f1303f0fc611f364efa88f15ca13
Prem Nirmal <prem.nirmal88@gmail.com>
2013-05-08 00:24:22 -0400

    changes to view, added a layout html file but stylesheets pending

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Blog::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
   resources :articles
   root :to => 'articles#index'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
