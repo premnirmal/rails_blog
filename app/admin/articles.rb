8bc86d5e8927082be0c6ac7fb67216bae52dbe54
Prem Nirmal <prem.nirmal88@gmail.com>
2013-05-09 23:21:22 -0400

    Changed tag in admin form to dropdown list

--- app/admin/articles.rb
+++ app/admin/articles.rb
@@ -3,15 +3,13 @@ ActiveAdmin.register Article do
     f.inputs "New blog article" do
       f.input :title
       f.input :body
-
-      f.has_many :tags do |tag_form| # form for tags (many-many, nested attr)
-        tag_form.inputs :tags do
-          tag_form.input :name
-          tag_form.input :_destroy, :as => :boolean
+      f.has_many :tags do |t|
+        t.inputs :tags do
+          t.select :name, collection => Tag.all
         end        
       end
     end
-    f.buttons    
+
   end
 
 end
